aae69badb29f2ca2e8eafcdb41d66807574d3723
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-04-27 13:45:02 -0500

    Added reset function

--- script.js
+++ script.js
@@ -4,5 +4,14 @@ function setUpPage () {
 
   // initialize whatever variables you need and add your event listeners
   // you can (but probably shouldn't) define functions inside this function
+  document.getElementById("x-score").innerHTML = 0;
+  document.getElementById("o-score").innerHTML = 0;
 
-};
+}
+
+
+// function resetScore () {
+// 	alert("Hello");
+// }
+
+
