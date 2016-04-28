eb3af9142be0d4a2336cc930bee49d788a98a83f
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-04-28 08:57:24 -0500

    Added win arrays

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function removeClass(element, className) {
 // VARIABLES
 
 var playerTurn = 0
-var board = [["", "", ""], [""],[""],[""],];
+var board = [["", "", ""], ["","",""], ["", "",""]]
 
 // ----------------------------------------------------------------------- 
 
@@ -82,10 +82,19 @@ function playerMove () {
   
   playerTurn++;
 
-  
+  var playerWin = board[0][0] === xO && board[0][1] === xO && board[0][2] === xO //row 1
+  								board[1][0] === xO && board[1][1] === xO && board[1][2] === xO //row 2
+  								board[2][0] === xO && board[2][1] === xO && board[2][2] === xO //row 3
+  								board[0][0] === xO && board[1][0] === xO && board[2][0] === xO //col 1
+  								board[0][1] === xO && board[1][1] === xO && board[2][1] === xO //col 2
+  								board[0][2] === xO && board[1][2] === xO && board[2][2] === xO //col 3
+  								board[0][0] === xO && board[1][1] === xO && board[2][2] === xO //diag1
+  								board[0][2] === xo && board[1][1] === xO && board[2][0] === xO //diag2
+
+
   if(playerWin){
-  	var winX
   }
+
   if(playerTurn === 8){
   } 
   else {
