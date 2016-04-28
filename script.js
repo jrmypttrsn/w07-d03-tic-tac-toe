d3ec2385ca5fd3ad009c11de6b1d8c579a83bd41
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-04-27 21:47:43 -0500

    Added board array and some styling

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", setUpPage);
-
+// -----------------------------------------------------------------------
 function setUpPage () {
 
   // initialize whatever variables you need and add your event listeners
@@ -7,5 +7,77 @@ function setUpPage () {
   document.getElementById("x-score").innerHTML = 0;
   document.getElementById("o-score").innerHTML = 0;
 
+  var elements = document.getElementsByClassName("board-tile");
+	for (var i = 0; i < elements.length; i++){
+		elements[i].addEventListener("click", playerMove);
+	}
 }
 
+function addClass(element, className){
+  element.classList.add(className);
+}
+
+function removeClass(element, className){
+  element.classList.remove(className);
+}
+// -----------------------------------------------------------------------
+
+
+// function playerMove () {
+// 	alert("Warning");
+// }
+
+// VARIABLES
+
+var playerTurn = 0
+var board = [["", "", ""], [],[],[],];
+
+// ----------------------------------------------------------------------- 
+
+function playerMove () {
+	var xO = "";
+	if(playerTurn % 2 === 0){
+    xO = "X";
+  } else {
+    xO = "O";
+  }
+  this.innerHTML = xO;
+
+  this.removeEventListener("click", playerMove);
+
+  switch(this.id) {
+  case "tile-1-1":
+    board[0][0] = xO;
+    break;
+  case "tile-1-2":
+    board[0][1] = xO;
+    break;
+  case "tile-1-3":
+    board[0][2] = xO;
+    break;
+  case "tile-2-1":
+    board[1][0] = xO;
+    break;
+  case "tile-2-2":
+    board[1][1] = xO;
+    break;
+  case "tile-2-3":
+    board[1][2] = xO;
+    break;
+  case "tile-3-1":
+    board[2][0] = xO;
+    break;
+  case "tile-3-2":
+    board[2][1] = xO;
+    break;
+  case "tile-3-3":
+    board[2][2] = xO;
+    break;
+	}
+}
+
+// // // ready messages
+// function playerPrompt () {
+// 	var elements = document.getElementsByClassName("ready-message")
+// 	for (var i = 0)
+// }
